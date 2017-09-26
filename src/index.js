8c552411e09e0b55ac149a3b6b1f5f0e52318fdb
Sigurd Fosseng <sigurd@fosseng.net>
2017-09-26 02:19:00 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ type State = any;
 type MiddlewareAPI = { dispatch: Dispatch, getState(): State };
 type Injector = (api: MiddlewareAPI) => { [key: string]: any };
 
-const mergeResults = (...args) => (result, fn) => ({ ...result, ...fn(...args) });
+const mergeResults = (...args) => (result, fn) => ({
+  ...result,
+  ...fn(...args),
+});
 
-export default (...injectors: Array<Injector>) => ({ dispatch, getState }: MiddlewareAPI) =>
-  (next: Dispatch) => (action: any) => {
-    if (typeof action === 'function') {
-      const defaultProps = { dispatch, getState };
-      return action(injectors.reduce(mergeResults(defaultProps), defaultProps));
-    }
-    return next(action);
-  };
+export default (...injectors: Array<Injector>) => ({
+  dispatch,
+  getState,
+}: MiddlewareAPI) => (next: Dispatch) => (action: any) => {
+  if (typeof action === 'function') {
+    const defaultProps = { dispatch, getState };
+    return action(injectors.reduce(mergeResults(defaultProps), defaultProps));
+  }
+  return next(action);
+};
--- src/test.js
+++ src/test.js
@@ -5,7 +5,7 @@ import di from './index.js';
 
 const fn = () => function noop() {};
 
-test('passthrough middleware when action is not a function', (assert) => {
+test('passthrough middleware when action is not a function', assert => {
   const store = { dispatch: fn(), getState: fn() };
   const action = { type: 'NOOP' };
   const next = sinon.stub();
@@ -14,8 +14,7 @@ test('passthrough middleware when action is not a function', (assert) => {
   assert.end();
 });
 
-
-test('thunk with dependency injection', (assert) => {
+test('thunk with dependency injection', assert => {
   const store = { dispatch: sinon.spy(), getState: sinon.spy() };
   const config = { aValue: true };
   const getConfig = sinon.spy(({ dispatch, getState }) => {
@@ -34,9 +33,18 @@ test('thunk with dependency injection', (assert) => {
 
   assert.ok(next.notCalled, 'action is function, do not call next');
   assert.ok(getConfig.calledWith(store), 'getConfig called with store');
-  assert.ok(action.calledWith({ ...store, config }), 'dependency injection works');
-  assert.ok(store.dispatch.calledWith('getConfig'), 'injector can call dispatch');
-  assert.ok(store.getState.calledWith('getConfig'), 'injector can call getState');
+  assert.ok(
+    action.calledWith({ ...store, config }),
+    'dependency injection works'
+  );
+  assert.ok(
+    store.dispatch.calledWith('getConfig'),
+    'injector can call dispatch'
+  );
+  assert.ok(
+    store.getState.calledWith('getConfig'),
+    'injector can call getState'
+  );
   assert.ok(store.dispatch.calledWith('action'), 'thunk can call dispatch');
   assert.ok(store.getState.calledWith('action'), 'thunk can call getState');
   assert.equal(result, 'actionResult', 'returns result from action');

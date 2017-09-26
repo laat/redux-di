c59b72ee470b15063cfce3c9ea0ddff1ca2e3353
Sigurd Fosseng <sigurd@fosseng.net>
2017-09-26 02:11:43 +0200

    update deps

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ type Injector = (api: MiddlewareAPI) => { [key: string]: any };
 const mergeResults = (...args) => (result, fn) => ({ ...result, ...fn(...args) });
 
 export default (...injectors: Array<Injector>) => ({ dispatch, getState }: MiddlewareAPI) =>
-(next: Dispatch) => (action: any) => {
-  if (typeof action === 'function') {
-    const defaultProps = { dispatch, getState };
-    return action(injectors.reduce(mergeResults(defaultProps), defaultProps));
-  }
-  return next(action);
-};
+  (next: Dispatch) => (action: any) => {
+    if (typeof action === 'function') {
+      const defaultProps = { dispatch, getState };
+      return action(injectors.reduce(mergeResults(defaultProps), defaultProps));
+    }
+    return next(action);
+  };

b44b7d6a08d98f01abc8fd473b5c74d4e337148c
Sigurd Fosseng <sigurd.fosseng@sparebank1.no>
2017-10-27 09:47:32 +0200

    v2, a dropin replacement for redux-thunk.
    Same thunk-api, with better api for configuration

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-// @flow
-type Dispatch = (action: Object | any) => any;
-type State = any;
-type MiddlewareAPI = { dispatch: Dispatch, getState(): State };
-type Injector = (api: MiddlewareAPI) => { [key: string]: any };
+export default () => {
+  const staticArgs = {};
+  const dynamicArgs = {};
+  const middleware = ({ dispatch, getState }) => {
+    const extraArgument = { ...staticArgs };
+    Object.entries(dynamicArgs).forEach(([key, callback]) => {
+      Object.defineProperty(extraArgument, key, {
+        get() {
+          return callback({ dispatch, getState });
+        },
+        enumerable: true,
+      });
+    });
 
-const mergeResults = (...args) => (result, fn) => ({
-  ...result,
-  ...fn(...args),
-});
-
-export default (...injectors: Array<Injector>) => ({
-  dispatch,
-  getState,
-}: MiddlewareAPI) => (next: Dispatch) => (action: any) => {
-  if (typeof action === 'function') {
-    const defaultProps = { dispatch, getState };
-    return action(injectors.reduce(mergeResults(defaultProps), defaultProps));
-  }
-  return next(action);
+    return next => action => {
+      if (typeof action === 'function') {
+        return action(dispatch, getState, extraArgument);
+      }
+      return next(action);
+    };
+  };
+  middleware.withStatic = arg => {
+    Object.assign(staticArgs, arg);
+    return middleware;
+  };
+  middleware.withDynamic = arg => {
+    Object.assign(dynamicArgs, arg);
+    return middleware;
+  };
+  return middleware;
 };
--- src/test.js
+++ src/test.js
@@ -1,52 +1,91 @@
-// @flow
-import sinon from 'sinon';
-import test from 'tape';
-import di from './index.js';
+/* eslint-env jest */
+import diThunkMiddleware from '.';
 
-const fn = () => function noop() {};
-
-test('passthrough middleware when action is not a function', assert => {
-  const store = { dispatch: fn(), getState: fn() };
-  const action = { type: 'NOOP' };
-  const next = sinon.stub();
-  di()(store)(next)(action);
-  assert.ok(next.calledWith(action), 'calls next with action');
-  assert.end();
-});
-
-test('thunk with dependency injection', assert => {
-  const store = { dispatch: sinon.spy(), getState: sinon.spy() };
-  const config = { aValue: true };
-  const getConfig = sinon.spy(({ dispatch, getState }) => {
-    getState('getConfig');
-    dispatch('getConfig');
-    return { config };
+describe('di-thunks', () => {
+  const doDispatch = () => {};
+  const doGetState = () => {};
+  const store = { dispatch: doDispatch, getState: doGetState };
+  const createNextHandler = () => diThunkMiddleware()(store);
+  describe('as normal thunks', () => {
+    test('must return a function to handle action', () => {
+      const nextHandler = createNextHandler();
+      const actionHandler = nextHandler();
+      expect(actionHandler).toBeInstanceOf(Function);
+      expect(actionHandler).toHaveLength(1);
+    });
+    test('must run the given action function with dispatch and getState', done => {
+      const nextHandler = createNextHandler();
+      const actionHandler = nextHandler();
+      actionHandler((dispatch, getState) => {
+        expect(dispatch).toBe(doDispatch);
+        expect(getState).toBe(doGetState);
+        done();
+      });
+    });
+    test('must pass action to next if not a function', () => {
+      const nextHandler = createNextHandler();
+      const expected = 'redux';
+      const actionHandler = nextHandler(() => expected);
+      const outcome = actionHandler();
+      expect(outcome).toBe(expected);
+    });
+    test('must be invoced sync if a function', () => {
+      const nextHandler = createNextHandler();
+      const actionHandler = nextHandler();
+      let mutated = 0;
+      actionHandler(() => ++mutated);
+      expect(mutated).toBe(1);
+    });
+    test('must throw if argument is non-object', () => {
+      expect(diThunkMiddleware()).toThrow();
+    });
   });
-  const action = sinon.spy(({ dispatch, getState }) => {
-    getState('action');
-    dispatch('action');
-    return 'actionResult';
+  describe('with extra arguments', () => {
+    test('static argument', done => {
+      const staticArg = { foobar: true };
+      const nextHandler = diThunkMiddleware().withStatic(staticArg)(store);
+      const actionHandler = nextHandler();
+      actionHandler((dispatch, getState, arg) => {
+        expect(dispatch).toBe(doDispatch);
+        expect(getState).toBe(doGetState);
+        expect(arg).toEqual(staticArg);
+        done();
+      });
+    });
+    test('dynamic argument functions called with store properties', done => {
+      const dynamicArg = {
+        foobar: ({ dispatch, getState }) => {
+          expect(dispatch).toBe(doDispatch);
+          expect(getState).toBe(doGetState);
+          done();
+        },
+      };
+      const nextHandler = diThunkMiddleware().withDynamic(dynamicArg)(store);
+      const actionHandler = nextHandler();
+      actionHandler((dispatch, getState, arg) => {
+        // eslint-disable-next-line
+        arg.foobar; // must be accessed for the property getter to be called.
+      });
+    });
+    test('dynamic+static', done => {
+      const dynamicArg = {
+        dynamic: () => 'dynamic',
+      };
+      const staticArg = {
+        foobar: true,
+      };
+      const middleware = diThunkMiddleware()
+        .withStatic(staticArg)
+        .withDynamic(dynamicArg);
+      const nextHandler = middleware(store);
+      const actionHandler = nextHandler();
+      actionHandler((dispatch, getState, arg) => {
+        expect(dispatch).toBe(doDispatch);
+        expect(getState).toBe(doGetState);
+        expect(arg.foobar).toBe(staticArg.foobar);
+        expect(arg.dynamic).toBe('dynamic');
+        done();
+      });
+    });
   });
-  const next = sinon.stub();
-
-  const result = di(getConfig)(store)(next)(action);
-
-  assert.ok(next.notCalled, 'action is function, do not call next');
-  assert.ok(getConfig.calledWith(store), 'getConfig called with store');
-  assert.ok(
-    action.calledWith({ ...store, config }),
-    'dependency injection works'
-  );
-  assert.ok(
-    store.dispatch.calledWith('getConfig'),
-    'injector can call dispatch'
-  );
-  assert.ok(
-    store.getState.calledWith('getConfig'),
-    'injector can call getState'
-  );
-  assert.ok(store.dispatch.calledWith('action'), 'thunk can call dispatch');
-  assert.ok(store.getState.calledWith('action'), 'thunk can call getState');
-  assert.equal(result, 'actionResult', 'returns result from action');
-  assert.end();
 });
